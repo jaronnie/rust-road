47e47bc9baa26cd5c12b59ab04a2d274d35487eb
jaronnie <jaron@jaronnie.com>
2023-07-03 20:01:05 +0800

    feat(road): update http client

--- code/http-client/src/main.rs
+++ code/http-client/src/main.rs
@@ -4,23 +4,33 @@ use serde_json::value::Value;
 
 
 async fn get() -> Result<HashMap<String, String>, reqwest::Error>{
-    Ok(reqwest::get("http://127.0.0.1:9090/json").await?.json::<HashMap<String, String>>().await?)
+    let res = reqwest::get("http://127.0.0.1:9090/json")
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?;
+    Ok(res)
 }
 
 async fn post() -> Result<HashMap<String, Value>, reqwest::Error>{
-    // post 请求要创建client
+    // new client
     let client = reqwest::Client::new();
 
-    // 组装header
+    // header
     let mut headers = HeaderMap::new();
     headers.insert("Content-Type", "application/json".parse().unwrap());
 
-    // 组装要提交的数据
     let mut data = HashMap::new();
     data.insert("name", "jaronnie".to_string());
 
-    // 发起post请求并返回
-    Ok(client.post("http://127.0.0.1:9090/post").headers(headers).json(&data).send().await?.json::<HashMap<String, Value>>().await?)
+    // send
+    let res = client.post("http://127.0.0.1:9090/post")
+        .headers(headers)
+        .json(&data)
+        .send()
+        .await?
+        .json::<HashMap<String, Value>>()
+        .await?;
+    Ok(res)
 }
 
 #[tokio::main]
