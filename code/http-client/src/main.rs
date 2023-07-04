f588a618a947a7090fe6473d43cfcc909740caef
jaronnie <jaron@jaronnie.com>
2023-07-04 10:37:38 +0800

    feat(road): update http client

--- code/http-client/src/main.rs
+++ code/http-client/src/main.rs
@@ -1,17 +1,20 @@
 use std::collections::HashMap;
 use reqwest::header::HeaderMap;
 use serde_json::value::Value;
+use anyhow::{Context, Result};
 
 
-async fn get() -> Result<HashMap<String, String>, reqwest::Error>{
+async fn get() -> Result<HashMap<String, Value>, Box<dyn std::error::Error>>{
     let res = reqwest::get("http://127.0.0.1:9090/json")
-        .await?
-        .json::<HashMap<String, String>>()
-        .await?;
+        .await
+        .context("请求错误")?
+        .json::<HashMap<String, Value>>()
+        .await
+        .context("反序列化错误")?;
     Ok(res)
 }
 
-async fn post() -> Result<HashMap<String, Value>, reqwest::Error>{
+async fn post() -> Result<HashMap<String, Value>,Box<dyn std::error::Error>>{
     // new client
     let client = reqwest::Client::new();
 
@@ -27,9 +30,11 @@ async fn post() -> Result<HashMap<String, Value>, reqwest::Error>{
         .headers(headers)
         .json(&data)
         .send()
-        .await?
+        .await
+        .context("post meet error")?
         .json::<HashMap<String, Value>>()
-        .await?;
+        .await
+        .context("反序列化错误")?;
     Ok(res)
 }
 
